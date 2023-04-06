ea70c674ccbb197633ba9d6f3b41777e559da7ae
wickit7 <wickitimo@gmail.com>
2023-04-06 11:04:41 +0200

    fixed server_rest_url used for enable and manage cache if federated server not equal to portal server

--- publish/publish_service_portal.py
+++ publish/publish_service_portal.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     # path to a JSON input file or multiple JSON files
     paramFiles = sys.argv[1:]
     #paramFiles = [r'C:\Temp\tutorial\publish_citymaps_cache_test.json']
-
+    
     # path to the overall log file if there is more than one json input file (stored in the "Logs" folder in the directory of the Python script).
     overall_log_folder = os.path.join(os.path.dirname(__file__), "Logs")
 
@@ -441,7 +441,7 @@ if __name__ == "__main__":
             else:
                 logger.info(f'Create cache scheme with predefined parameters')
             # define correct url's
-            server_rest_url = portal_url.replace("portal", "server/rest")
+            server_rest_url = f'{federated_server_url}' + r'/rest'
             rest_service = service[0].split("arcgis")
             service_url = server_rest_url + rest_service[1]
             try:
